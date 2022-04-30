c4a53c527cba3924370f6b4e2ba6deed07f76224
rafaelrodrigopa <rafael.rodrigo.p.a@gmail.com>
2022-04-29 21:23:30 -0300

    TextField & DatePicker

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -1,14 +1,18 @@
 package gui.util;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Locale;
 
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
 
 public class Utils {
 
@@ -47,21 +51,49 @@ public class Utils {
 
 	public static <T> void formatTableColumnDouble(TableColumn<T, Double> tableColumn, int decimalPlaces) {
 		tableColumn.setCellFactory(column -> {
-			TableCell<T, Double> cell = new TableCell<T, Double>(){
-				
+			TableCell<T, Double> cell = new TableCell<T, Double>() {
+
 				@Override
 				protected void updateItem(Double item, boolean empty) {
 					super.updateItem(item, empty);
-					if(empty) {
+					if (empty) {
 						setText(null);
-					}else {
+					} else {
 						Locale.setDefault(Locale.US);
-						setText(String.format("%."+	decimalPlaces+"f", item));
+						setText(String.format("%." + decimalPlaces + "f", item));
 					}
 				}
-				
+
 			};
 			return cell;
+		});
+	}
+
+	public static void formatDatePicker(DatePicker datePicker, String format) {
+		datePicker.setConverter(new StringConverter<LocalDate>() {
+			DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(format);
+			{
+				datePicker.setPromptText(format.toLowerCase());
+			}
+
+			@Override
+			public String toString(LocalDate date) {
+				if (date != null) {
+					return dateFormatter.format(date);
+				} else {
+					return "";
+				}
+			}
+
+			@Override
+			public LocalDate fromString(String string) {
+				if (string != null && !string.isEmpty()) {
+					return LocalDate.parse(string, dateFormatter);
+				} else {
+					return null;
+				}
+			}
+
 		});
 	}
 
